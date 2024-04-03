a2dc58c06bc348c17c043584235992ed399b85c6
bar23354 <bar23354@uvg.edu.gt>
2024-04-03 09:02:15 -0600

    Estructura inicial planteada

---
+++ ElPilar.java
@@ -0,0 +1,10 @@
+/*
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos - sección 40
+ * Roberto Barreda - 23354
+ */
+
+public class ElPilar {
+    public static void main(String[] args) {
+    } 
+}
---
+++ Paciente.java
@@ -0,0 +1,9 @@
+/*
+ * Universidad del Valle de Guatemala
+ * Algoritmos y Estructuras de Datos - sección 40
+ * Roberto Barreda - 23354
+ */
+
+public class Paciente {
+
+}
